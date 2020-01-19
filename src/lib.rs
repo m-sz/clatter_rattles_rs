9cea1f496ea1dce0a0667cdf3e282932ce1bbb60
bartossh <lenart.consulting@gmail.com>
2020-01-19 19:06:01 +0100

    add test for full sample

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,31 @@ mod tests {
     use std::thread::sleep;
     
     #[test]
-    fn test_calc_fingerprint_collection() {
+    fn test_calc_fingerprint_collection_short() {
         let start_time = Instant::now();
         let fingerprint_handler = super::fingerprint::FingerprintHandle::new();
         let fingerprint_collection = fingerprint_handler.calc_fingerprint_collection(
             &super::helpers::decode_mp3(&format!("./assets/sample.mp3")).unwrap()
         ).unwrap();
         for fingerprint in fingerprint_collection.iter() {
-            assert_eq!(*fingerprint > 10000000001, true);
+            if *fingerprint != 0 {
+                assert_eq!(*fingerprint > 10000000001, true);
+            }
+        }
+        println!("Decoding and hashing stream took {} milliseconds", start_time.elapsed().as_millis());
+        println!("Number of fingerprints in collection is {}", &fingerprint_collection.len());
+    }
+    #[test]
+    fn test_calc_fingerprint_collection_long() {
+        let start_time = Instant::now();
+        let fingerprint_handler = super::fingerprint::FingerprintHandle::new();
+        let fingerprint_collection = fingerprint_handler.calc_fingerprint_collection(
+            &super::helpers::decode_mp3(&format!("./assets/red_hot_chili_peppers_dark_necessities.mp3")).unwrap()
+        ).unwrap();
+        for fingerprint in fingerprint_collection.iter() {
+            if *fingerprint != 0 {
+                assert_eq!(*fingerprint > 10000000001, true);
+            }
         }
         println!("Decoding and hashing stream took {} milliseconds", start_time.elapsed().as_millis());
         println!("Number of fingerprints in collection is {}", &fingerprint_collection.len());
