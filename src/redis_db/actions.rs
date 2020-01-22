dc7f05685b135528761935c994db2d6411c32c9e
bartossh <lenart.consulting@gmail.com>
2020-01-22 15:11:17 +0100

    add benchmark test for repository handle to redis db

--- src/redis_db/actions.rs
+++ src/redis_db/actions.rs
@@ -24,6 +24,7 @@ impl RedisHelper {
 }
 
 impl Repository for RedisHelper {
+
     fn store(&mut self, fingerprints: &Vec<usize>, song: &String) -> Result<(), Box<dyn Error>> {
         transaction(&mut self.connection, fingerprints, |con, pipe| {
             for fingerprint in fingerprints.iter() {
@@ -61,6 +62,7 @@ impl Repository for RedisHelper {
 #[cfg(test)]
 mod test {
     use super::{RedisHelper, Repository};
+    use std::time::{Instant};
     #[test]
     fn test_repository() {
         let fake_fingerprints_1: Vec<usize> = vec![
@@ -96,5 +98,47 @@ mod test {
         } else {
             assert_eq!(1, 2);
         }
+    }
+    #[test]
+    fn test_benchmark_repository() {
+        let mut fake_fingerprints_1: Vec<usize> = Vec::new();
+        let mut fake_fingerprints_2: Vec<usize> = Vec::new();
+        let fingerprint_1_max: usize = 20000;
+        let fingerprint_2_max: usize = 30000;
+        for fingerprint in 0..fingerprint_1_max {
+            fake_fingerprints_1.push(fingerprint);
+        }
+        for fingerprint in 0..fingerprint_2_max {
+            fake_fingerprints_2.push(fingerprint);
+        } 
+        let song_1 = format!("Shrek and Donkey - Pinocchio is laying again");
+        let song_2 = format!("Alice in Wonderland - Poker face");
+        let mut db_handler = RedisHelper::new(&"redis://127.0.0.1/").unwrap();
+        let start_time = Instant::now();
+        if let Ok(_) = db_handler.store(&fake_fingerprints_1, &song_1) {
+            assert_eq!(1, 1);
+        } else {
+            assert_eq!(1, 2);
+        }
+        if let Ok(_) = db_handler.store(&fake_fingerprints_2, &song_2) {
+            assert_eq!(1, 1);
+        } else {
+            assert_eq!(1, 2);
+        }
+        println!("Adding to database of 2 songs of total {} fingerprints took {} milliseconds", fingerprint_1_max + fingerprint_2_max, start_time.elapsed().as_millis());
+        let half_time = Instant::now();
+        if let Ok(matches) = db_handler.find_matches(&fake_fingerprints_2) {
+            match matches.get(&song_1) {
+                Some(m) => assert_eq!(*m, fingerprint_1_max),
+                None => assert_eq!(1, 2)
+            };
+            match matches.get(&song_2) {
+                Some(m) => assert_eq!(*m, fingerprint_2_max),
+                None => assert_eq!(1, 2)
+            };
+        } else {
+            assert_eq!(1, 2);
+        }
+        println!("Matching of total {} fingerprints took {} milliseconds", fingerprint_1_max + fingerprint_2_max, start_time.elapsed().as_millis());
     }
 }
